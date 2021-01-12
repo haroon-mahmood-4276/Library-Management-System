74b2e6c9ddfaafd50054a86d6302d37c375f7ff0
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2021-01-12 10:07:01 +0500

    Seeder Fix

--- database/seeders/UserData.php
+++ database/seeders/UserData.php
@@ -18,14 +18,14 @@ class UserData extends Seeder
         DB::table('users')->insert([
             [
                 'email' => 'admin@lms.com',
-                'passowrd' => Hash::make('123456'),
+                'password' => Hash::make('123456'),
                 'first_name' => 'Haroon',
                 'last_name' => 'Mahmood',
                 'user_role' => 'Admin',
             ],
             [
                 'email' => 'user@lms.com',
-                'passowrd' => Hash::make('123456'),
+                'password' => Hash::make('123456'),
                 'first_name' => 'Haroon',
                 'last_name' => 'Mahmood',
                 'user_role' => 'User',
